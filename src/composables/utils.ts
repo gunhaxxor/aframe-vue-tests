488d1f63a1ebab1915fb130ccd91787e02549889
Martin Törnros <martin.tornros@ri.se>
2024-06-07 13:08:52 +0200

    Simulate more Oculus buttons on keyboard

--- src/composables/utils.ts
+++ src/composables/utils.ts
@@ -12,14 +12,32 @@ export function simulateOculus() {
             oculusHandSimulator.value['hands-active'] = !oculusHandSimulator.value['hands-active']
         }
         if (oculusHandSimulator.value['hands-active']) {
+            if (e.key === 'a') {
+                oculusButtons.value.x = true
+            }
+            if (e.key === 'b') {
+                oculusButtons.value.x = true
+            }
             if (e.key === 'x') {
+                oculusButtons.value.x = true
+            }
+            if (e.key === 'y') {
                 oculusButtons.value.x = true
             }
         }
     })
     window.onkeyup = ((e: KeyboardEvent) => {
         if (oculusHandSimulator.value['hands-active']) {
+            if (e.key === 'a') {
+                oculusButtons.value.x = false
+            }
+            if (e.key === 'b') {
+                oculusButtons.value.x = false
+            }
             if (e.key === 'x') {
+                oculusButtons.value.x = false
+            }
+            if (e.key === 'y') {
                 oculusButtons.value.x = false
             }
         }
